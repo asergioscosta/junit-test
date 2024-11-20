f4a0eaaf8953e7e8ca024445ba9309dbd87a0f6a
Augusto Sergio <asergioscosta@gmail.com>
2024-11-20 16:30:01 -0300

    Adicionar teste para número negativo que não é primo.

--- testeunidade/src/test/java/org/testeunidade/numeroprimo/NumeroPrimoTest.java
+++ testeunidade/src/test/java/org/testeunidade/numeroprimo/NumeroPrimoTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class NumeroPrimoTest {
 
     @Test
-    void deveRetornarNumeroPrimo(){
+    void deveRetornarNumeroPrimo() {
         NumeroPrimo numeroPrimo = new NumeroPrimo();
         numeroPrimo.setNumero(5);
 
@@ -15,16 +15,23 @@ class NumeroPrimoTest {
     }
 
     @Test
-    void deveRetornarNumeroNaoPrimo(){
+    void deveRetornarNumeroNaoPrimo() {
         NumeroPrimo numeroPrimo = new NumeroPrimo();
         numeroPrimo.setNumero(1);
         assertEquals("Não é primo", numeroPrimo.validarNumeroPrimo());
     }
 
     @Test
-    void deveRetornarNumeroNaoPrimo0(){
+    void deveRetornarNumeroNaoPrimo0() {
         NumeroPrimo numeroPrimo = new NumeroPrimo();
         numeroPrimo.setNumero(0);
+        assertEquals("Não é primo", numeroPrimo.validarNumeroPrimo());
+    }
+
+    @Test
+    void deveRetornarNumeroNaoPrimoNegativo() {
+        NumeroPrimo numeroPrimo = new NumeroPrimo();
+        numeroPrimo.setNumero(-1);
         assertEquals("Não é primo", numeroPrimo.validarNumeroPrimo());
     }
 }
